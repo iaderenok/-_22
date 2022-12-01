2014d2c9de3de0aac8287e88aa81773e8703684c
iaderenok <63782816+iaderenok@users.noreply.github.com>
2022-12-01 16:51:09 +0500

    Add files via upload
    добавил задание

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int count = 0;
+string[] arr = { "13465", "2666", "333", "765" };
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"длина второго массива -> [{count}]");
